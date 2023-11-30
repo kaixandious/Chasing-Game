09a52706e8612059aff2cdbeed6a73b76b62d8d5
hanayataiga0704@gmail.com <hanayataiga0704@gmail.com>
2023-12-01 00:37:45 +0700

    fifth push
    done change level

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
-
+    private bool LevelCompleted = false;
 
     private void Start()
     {
@@ -14,15 +15,15 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !LevelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
